81e72fc19b3fc2389725f336759ed1347ec16e12
maya garcia <maya.garciabo@gmail.com>
2024-06-03 21:51:46 -0600

    yelp mvp done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_reviews_path(@restaurant)
     else
       render :new, status: :unprocessable_entity
     end
